080d5c70df2f2784d08c1b531f4d98ec532a9f16
OfficialPooka <65380887+OfficialPooka@users.noreply.github.com>
2024-03-06 18:13:54 -0800

    ver 0.3.0
    new header, new clock, going to fix with javascript. header still needs to fly in when clicking enter

--- script.js
+++ script.js
@@ -26,25 +26,67 @@ enter.onclick = function loadshit() {
         // makes enter button disappear
     if (loaded < 1) {
         document.getElementById("enter-fx").style.animation = "enter-fx";
-        document.getElementById("enter-fx").style.animationDuration = "1s";
+        document.getElementById("enter-fx").style.animationDuration = "0.3s";
         document.getElementById("enter-fx").style.animationFillMode = "forwards";
         document.getElementById("enter-fx").style.animationTimingFunction = "cubic-bezier(0,.54,0,.99)"
         enter.style.animation = "outro-button";
-        enter.style.animationDuration = "1.5s";
+        enter.style.animationDuration = "0.5s";
         enter.style.animationFillMode = "forwards";
 
-        //playes sfx
+        // plays sfx
         click.play();
         
         // move shit into frame
-        document.getElementById("navbar").style.bottom = 0;
+        // document.getElementById("navbar").style.bottom = 0;
 
         // move shit out of frame 
         document.getElementById("title").style.top = "-25%";
         document.getElementById("title-bg").style.top = "-25%";
         document.getElementById("bottom-text").style.bottom = "-25%";
 
-        console.log("big dicks");
+        
         loaded++;   
+        console.log(loaded);
     }
 };
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+    }
+    
+    m = (m < 10) ? "0" + m : m;
+
+    var time = h + ":" + m;
+    document.getElementById("time").innerText = time;
+    document.getElementById("time").textContent = time;
+    
+    hRotation = 30 * h + m / 2; //converting current time
+    mRotation = 6 * m;
+
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+
+    setTimeout(showTime, 17000); // setting it as something 60k isnt divisible by makes it have a chance of updating sooner than later as opposed to updating at the same offset every time like with 20 seconds
+}
+
+showTime();
+
+i = 0
+
+function rotateTest() {
+    i++
+        hour.style.transform = `rotate(${i*2}deg)`; 
+        minute.style.transform = `rotate(${i*3}deg)`;
+    setTimeout(rotateTest, 10);
+}
+
+//rotateTest();
